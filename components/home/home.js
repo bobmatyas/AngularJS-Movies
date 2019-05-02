209cbfe2c402f8164ee587d064a202106dffc88b
Marion Luxem <marion@scottallencreative.com>
2019-05-02 18:49:16 -0400

    added search filters and labels

--- components/home/home.js
+++ components/home/home.js
@@ -4,7 +4,6 @@ function HomeController(MovieService, $q) {
       // List of movies to parse    
       ctrl.search = [];
 
-
       // ctrl.show = false;
 
       //ctrl.watchList = [];
@@ -36,7 +35,10 @@ function HomeController(MovieService, $q) {
                 let childObj = {
                  movie_id: child.id,
                  movie_title: child.title,
-                 movie_poster: child.poster_path 
+                 movie_poster: child.poster_path, 
+                 movie_vote_average: child.vote_average,
+                 movie_release_date: child.release_date,
+                 movie_original_language: child.original_language,
                }
 
               ctrl.search.push(childObj);
@@ -62,13 +64,34 @@ function HomeController(MovieService, $q) {
 
     <h3>Search Movies  (home.js)</h3>
   
-<div class="containter">
-  <input type="text" ng-model="homeSearch" placeholder="Search" />
+<div class="container">
+  <input type="text" ng-model="homeSearch" placeholder="Search by Title" />
+  <label>Sort by Popularity: <select id="popularity">
+    <option value="default">Default</option>
+    <option value="ascending">Ascending</option>
+    <option value="descending">Descending</option>
+  </select></label>
+
+  <label>Sort by Release Date: <select id="release-date">
+    <option value="default">Default</option>  
+    <option value="ascending">Ascending</option>
+    <option value="descending">Descending</option>
+  </select></label>
+
+  <label>Sort by Movie Length: <select id="run-time">
+    <option value="default">Default</option>
+    <option value="ascending">Ascending</option>
+    <option value="descending">Descending</option>
+  </select></label>
     <div class="search-result-container">
     <!--   -->
-      <div class="search-result-container-item" ng-repeat="post in $ctrl.search | filter: homeSearch">
+      <div class="search-result-container-item" ng-repeat="post in $ctrl.search | filter: homeSearch | orderBy: 'movie_title'">
         <div class="search-result-photo"><img src="https://image.tmdb.org/t/p/w185_and_h278_bestv2/{{post.movie_poster}}" /> </div> <div class="search-result-contents"><h2>{{post.movie_title}}</h2>
-          <i class="far fa-heart" ng-click="$ctrl.addToWatchList(post.movie_id)">Add to Favorites</i> </div>
+          <i class="far fa-heart" ng-click="$ctrl.addToWatchList(post.movie_id)">Add to Favorites</i>
+          <p>Popularity: {{post.movie_vote_average}}</p>
+          <p>Release Date: {{post.movie_release_date}}</p>
+          <p>Original Language: {{post.movie_original_language}}</p>
+          </div>
           <!--- fas fa-heart for solid -->
         </div>
     </div>
