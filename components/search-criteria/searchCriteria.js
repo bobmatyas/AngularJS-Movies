bbeffcd1d042c2d28640ba553ee93c597e60b1f6
bobmatyas <ballium@protonmail.com>
2019-05-03 10:32:52 -0500

    setup a working version of sort by title in searchResults. Will setup child after everything works

--- components/search-criteria/searchCriteria.js
+++ components/search-criteria/searchCriteria.js
@@ -14,10 +14,11 @@ function SearchCriteriaController() {
     <div class="container">
         <input type="text" ng-model="$ctrl.homeSearch" placeholder="Search by Title" />
         <label>Sort by Popularity: <select id="popularity">
-          <option value="default">Default</option>
-          <option value="ascending">Ascending</option>
-          <option value="descending">Descending</option>
-        </select></label>
+        <option value="default">Default</option>
+        <option value="ascending">Ascending</option>
+        <option value="descending">Descending</option>
+      </select></label>
+
       
         <label>Sort by Release Date: <select id="release-date">
           <option value="default">Default</option>  
--- components/search-results/searchResults.js
+++ components/search-results/searchResults.js
@@ -1,81 +1,104 @@
 function SearchResultsController(MovieService, $q) {
-    var ctrl = this;
+  var ctrl = this;
 
-    ctrl.fetchMovies = (search) => {
-        Service.fetchMovies(search)
-            .then( (movies) => {
-                ctrl.movies = movies
-            })
+  ctrl.fetchMovies = (search) => {
+    Service.fetchMovies(search)
+      .then((movies) => {
+        ctrl.movies = movies
+      })
+  }
+
+  // List of movies to parse    
+  ctrl.search = [];
+
+  // ctrl.show = false;
+
+  //ctrl.watchList = [];
+
+  console.log(MovieService.watchList);
+  ctrl.addToWatchList = (movieId) => {
+    console.log(`add to watch list clicked`);
+    if (MovieService.watchList.length >= 1) {
+      doesExist = MovieService.watchList.includes(movieId);
+      if (doesExist === false) {
+        MovieService.watchList.push(movieId);
+      }
+    } else {
+      MovieService.watchList.push(movieId);
     }
+    console.log(`current watch list: ${MovieService.watchList}`);
+  }
 
- // List of movies to parse    
- ctrl.search = [];
+  ctrl.fetchMovies = (search) => {
+    // Call service, then set our data
+    console.log("This was clicked");
+    return $q(function (resolve, reject) {
+      MovieService.fetchMovies(search)
+        .then((response) => {
 
- // ctrl.show = false;
+          let results = response;
+          //console.log(response);
+          ctrl.search = [];
+          response.data.results.forEach(function (child) {
+            let childObj = {
+              movie_id: child.id,
+              movie_title: child.title,
+              movie_poster: child.poster_path,
+              movie_popularity: child.popularity,
+              movie_release_date: child.release_date,
+              movie_original_language: child.original_language,
+            }
 
- //ctrl.watchList = [];
+            ctrl.search.push(childObj);
 
- console.log(MovieService.watchList);
- ctrl.addToWatchList = (movieId) => {
-   console.log(`add to watch list clicked`);
-   if (MovieService.watchList.length >= 1) {
-     doesExist = MovieService.watchList.includes(movieId);
-     if (doesExist === false) {
-       MovieService.watchList.push(movieId);
-     }
-   } else {
-       MovieService.watchList.push(movieId);
-   }
-   console.log(`current watch list: ${MovieService.watchList}`);  
- }
+            if (child === (results.length - 1)) {
+              resolve();
+            }
+          });
 
- ctrl.fetchMovies = (search) => {
-   // Call service, then set our data
-   console.log("This was clicked");
-   return $q(function(resolve, reject) {
-     MovieService.fetchMovies(search)
-       .then( (response) => {
+        });
+    });
+  };
 
-         let results = response;
-         //console.log(response);
-         ctrl.search = [];
-         response.data.results.forEach( function(child) {
-           let childObj = {
-            movie_id: child.id,
-            movie_title: child.title,
-            movie_poster: child.poster_path, 
-            movie_popularity: child.popularity,
-            movie_release_date: child.release_date,
-            movie_original_language: child.original_language,
-          }
+  ctrl.propertyName = 'movie_popularity';
+  ctrl.reverse = false;
 
-         ctrl.search.push(childObj);
+  ctrl.sortBy = function(propertyName) {
+    console.log(`sort clicked`);
+    console.log(propertyName);
+    ctrl.reverse = (ctrl.propertyName === propertyName) ? !ctrl.reverse : false;
+    ctrl.propertyName = propertyName;
+    console.log(ctrl.reverse);
+    console.log(propertyName);
+  };
 
-         if ( child === (results.length -1) ) {
-           resolve();
-        }
-     });
-
-     });
-   });
- };
-
-
-
-
+  console.log(ctrl.propertyName);
 }
-  
-  angular.module('MovieApp').component('searchResults', {
-    template: `
+
+angular.module('MovieApp').component('searchResults', {
+  template: `
         <section id="search-results">
             
-            
+
+      <label>Sort by Title: 
+        <select id="title" ng-change="$ctrl.sortBy('movie_title')" ng-model="sort_by_title">
+          <option value="ascending" ng-click="$ctrl.sortBy('movie_title')">A-Z</option>
+          <option value="descending" ng-click="$ctrl.sortBy('movie_title')">Z-A</option>
+        </select>
+      </label>
+      
+      <br>
+      <button ng-click="$ctrl.sortBy('movie_title')">Title</button>
+        <p class="sortorder" ng-show="$ctrl.propertyName === 'movie_title'" ng-class="{reverse: reverse}"> reverse</p>
+
                 
             <search-criteria fetch-movies="$ctrl.fetchMovies(search)"></search-criteria>
 
+
+
     <div class="search-result-container">
     <!--   -->
-      <div class="search-result-container-item" ng-repeat="post in $ctrl.search | orderBy: 'movie_title'">
+      <div class="search-result-container-item" ng-repeat="post in $ctrl.search | orderBy: $ctrl.propertyName:$ctrl.reverse">
         <div class="search-result-photo"><img src="https://image.tmdb.org/t/p/w185_and_h278_bestv2/{{post.movie_poster}}" /> </div> <div class="search-result-contents"><h2>{{post.movie_title}}</h2>
           <i class="far fa-heart" ng-click="$ctrl.addToWatchList(post.movie_id)">Add to Favorites</i>
           <p>Popularity: {{post.movie_vote_average}}</p>
@@ -87,5 +110,5 @@ function SearchResultsController(MovieService, $q) {
     </div>
 
         </section>`, // or use templateUrl
-    controller: SearchResultsController
-  });
+  controller: SearchResultsController
+});
